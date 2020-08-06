9b7e94dee9ce7b2ae43ca9747c3e80f1ec0448b6
auralshin <anshulspartan141@gmail.com>
2020-08-07 00:43:35 +0530

    changes : - imported three js files - added a 3D  box with rotation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState, useRef } from "react";
+import "./App.css";
+import { orbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { Canvas, extend } from "react-three-fiber";
+import { useSpring, a } from 'react-spring/three'
 
+extend({orbitControls});
 
-function App() {
+const App = () => {
+
+  const [hovered, setHovered] = useState(false);
+  const [active, setActive] = useState(false);
+  const props = useSpring({
+    scale : active ? [1.5,1.5,1.5] : [1,1,1]
+  })
+
   return (
-    <div className="App">
-      Learning Using fluid animation in React
-    </div>
+    <a.mesh
+    
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+      onClick={() => setActive(!active)}
+      scale={props.scale}
+    >
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshBasicMaterial
+        attach="material"
+        color={hovered ? "hotpink" : "red"}
+      />
+    </a.mesh>
   );
-}
+};
 
-export default App;
+export default () => (
+  <Canvas>
+    <App />
+  </Canvas>
+);
