84803ec368eba55022a497edad67247c1f909698
auralshin <anshulspartan141@gmail.com>
2020-08-07 16:30:39 +0530

    changes: - imported required lib - used orbit controls - created a plan - used animated mesh and fog effect of react spring - updated cast shadow

--- src/App.js
+++ src/App.js
@@ -1,38 +1,66 @@
 import React, { useState, useRef } from "react";
 import "./App.css";
-import { orbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { Canvas, extend } from "react-three-fiber";
-import { useSpring, a } from 'react-spring/three'
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { Canvas, extend, useThree, useFrame } from "react-three-fiber";
+import { useSpring, a } from "react-spring/three";
 
-extend({orbitControls});
+extend({ OrbitControls });
 
-const App = () => {
+const Controls = () => {
+  const orbitRef = useRef();
+  const { camera, gl } = useThree();
 
-  const [hovered, setHovered] = useState(false);
-  const [active, setActive] = useState(false);
-  const props = useSpring({
-    scale : active ? [1.5,1.5,1.5] : [1,1,1]
-  })
+  useFrame(() => {
+    orbitRef.current.update();
+  });
 
   return (
-    <a.mesh
-    
-      onPointerOver={() => setHovered(true)}
-      onPointerOut={() => setHovered(false)}
-      onClick={() => setActive(!active)}
-      scale={props.scale}
-    >
+    <orbitControls
+      autoRotate
+      maxPolarAngle={Math.PI / 3}
+      minPolarAngle={Math.PI / 3}
+      args={[camera, gl.domElement]}
+      ref={orbitRef}
+    />
+  );
+};
+
+const Plane = () => (
+  <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]} receiveShadow>
+    <planeBufferGeometry attach="geometry" args={[100, 100]} />
+    <meshPhysicalMaterial attach="material" color="white" />
+  </mesh>
+);
+
+const App = () => {
+  const [active, setActive] = useState(false);
+  const props = useSpring({
+    scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
+    color: "grey",
+  });
+
+  return (
+    <a.mesh onClick={() => setActive(!active)} scale={props.scale} castShadow>
+      <Controls />
+      <ambientLight />
+      <spotLight position={[0, 5, 5]} penumbra={1} castShadow />
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshBasicMaterial
-        attach="material"
-        color={hovered ? "hotpink" : "red"}
-      />
+      <a.meshPhysicalMaterial attach="material" color={props.color} />
     </a.mesh>
   );
 };
 
 export default () => (
-  <Canvas>
+  <Canvas
+    camera={{ position: [0, 0, 5] }}
+    onCreated={({ gl }) => {
+      gl.shadowMap.enabled = true;
+      gl.shadowMap.type = THREE.PCFSoftShadowMap;
+    }}
+  >
+    <fog attach="fog" args={["white", 5, 15]} />
     <App />
+    <Plane />
   </Canvas>
 );
