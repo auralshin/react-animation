b881e8905ce5802e437fe530322f240f10781b2f
auralshin <anshulspartan141@gmail.com>
2020-08-23 18:24:45 +0530

    changes: - import drei addon for 3js - spinning shadow - animated mesh - shaadow managed - planebuffergeometry updated

--- src/App.js
+++ src/App.js
@@ -1,66 +1,101 @@
-import React, { useState, useRef } from "react";
-import "./App.css";
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { Canvas, extend, useThree, useFrame } from "react-three-fiber";
+import React, { useRef, useState } from "react";
+//R3F
+import { Canvas, useFrame } from "react-three-fiber";
+// Deai - R3F
+import { softShadows, MeshWobbleMaterial, OrbitControls } from "drei";
+
+// Styles
+import "./App.scss";
+// React Spring
 import { useSpring, a } from "react-spring/three";
 
-extend({ OrbitControls });
+// soft Shadows
+softShadows();
 
-const Controls = () => {
-  const orbitRef = useRef();
-  const { camera, gl } = useThree();
+const SpinningMesh = ({ position, color, speed, args }) => {
+  //ref to target the mesh
+  const mesh = useRef();
 
-  useFrame(() => {
-    orbitRef.current.update();
-  });
+  //useFrame allows us to re-render/update rotation on each frame
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
 
-  return (
-    <orbitControls
-      autoRotate
-      maxPolarAngle={Math.PI / 3}
-      minPolarAngle={Math.PI / 3}
-      args={[camera, gl.domElement]}
-      ref={orbitRef}
-    />
-  );
-};
-
-const Plane = () => (
-  <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]} receiveShadow>
-    <planeBufferGeometry attach="geometry" args={[100, 100]} />
-    <meshPhysicalMaterial attach="material" color="white" />
-  </mesh>
-);
-
-const App = () => {
-  const [active, setActive] = useState(false);
+  //Basic expand state
+  const [expand, setExpand] = useState(false);
+  // React spring expand animation
   const props = useSpring({
-    scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
-    color: "grey",
+    scale: expand ? [1.4, 1.4, 1.4] : [1, 1, 1],
   });
-
   return (
-    <a.mesh onClick={() => setActive(!active)} scale={props.scale} castShadow>
-      <Controls />
-      <ambientLight />
-      <spotLight position={[0, 5, 5]} penumbra={1} castShadow />
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <a.meshPhysicalMaterial attach="material" color={props.color} />
+    <a.mesh
+      position={position}
+      ref={mesh}
+      onClick={() => setExpand(!expand)}
+      scale={props.scale}
+      castShadow
+    >
+      <boxBufferGeometry attach="geometry" args={args} />
+      <MeshWobbleMaterial
+        color={color}
+        speed={speed}
+        attach="material"
+        factor={0.6}
+      />
     </a.mesh>
   );
 };
 
-export default () => (
-  <Canvas
-    camera={{ position: [0, 0, 5] }}
-    onCreated={({ gl }) => {
-      gl.shadowMap.enabled = true;
-      gl.shadowMap.type = THREE.PCFSoftShadowMap;
-    }}
-  >
-    <fog attach="fog" args={["white", 5, 15]} />
-    <App />
-    <Plane />
-  </Canvas>
-);
+const App = () => {
+  return (
+    <>
+     
+      {/* Our Scene & Camera is already built into our canvas */}
+      <Canvas
+        colorManagement
+        shadowMap
+        camera={{ position: [-5, 2, 10], fov: 60 }}
+      >
+        {/* This light makes things look pretty */}
+        <ambientLight intensity={0.3} />
+        {/* Our main source of light, also casting our shadow */}
+        <directionalLight
+          castShadow
+          position={[0, 10, 0]}
+          intensity={1.5}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+        {/* A light to help illumnate the spinning boxes */}
+        <pointLight position={[-10, 0, -20]} intensity={0.5} />
+        <pointLight position={[0, -10, 0]} intensity={1.5} />
+        <group>
+          {/* This mesh is the plane (The floor) */}
+          <mesh
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, -3, 0]}
+            receiveShadow
+          >
+            <planeBufferGeometry attach="geometry" args={[100, 100]} />
+            <shadowMaterial attach="material" opacity={0.3} />
+          </mesh>
+          <SpinningMesh
+            position={[0, 1, 0]}
+            color="black"
+            args={[3, 2, 1]}
+            speed={2}
+          />
+          <SpinningMesh position={[-2, 1, -5]} color="red" speed={6} />
+          <SpinningMesh position={[5, 1, -2]} color="red" speed={6} />
+        </group>
+        {/* Allows us to move the canvas around for different prespectives */}
+        <OrbitControls />
+      </Canvas>
+    </>
+  );
+};
+
+export default App;
