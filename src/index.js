798d6f036e50a2cf5eb5798e373b5c411837e973
auralshin <anshulspartan141@gmail.com>
2020-09-28 07:13:54 +0530

    router added

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import Load from './components/loader'
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Loader from "./Loader";
+import Navbar from "./Navbar";
+import Game from "./Game";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    <Load />
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={App}></Route>
+        <Route path="/game" exact component={Game}></Route>
+        <Route path="/loader" exact component={Loader}></Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
